4cee67401ea021138ffd928458c333297a6e59b0
Kianoush Haratiannejadi <haratiank2@gmail.com>
2023-12-11 10:35:41 -0500

    debug

--- cost_of_living_EDA.py
+++ cost_of_living_EDA.py
@@ -78,6 +78,7 @@ def color_producer(val):
         return 'black'
 
 
+# Price of:  Taxi 1hour Waiting (Normal Tariff)
 m_1 = folium.Map(location=[df.lat.mean(),df.lon.mean()], tiles='cartodbpositron', zoom_start=2)
 
 item = top_range[0]
@@ -94,7 +95,7 @@ print ('Price of: ', item)
 m_1.show_in_browser()
 
 
-
+#Price of:  Price per Square Meter to Buy Apartment Outside of Centre
 m_2= folium.Map(location=[df.lat.mean(),df.lon.mean()], tiles='cartodbpositron', zoom_start=2)
 item = top_range[2]
 
@@ -108,7 +109,7 @@ for i in range(0,len(df)):
 print ('Price of: ', item)
 
 
-
+# Price of:  Average Monthly Net Salary (After Tax)
 m_3= folium.Map(location=[df.lat.mean(),df.lon.mean()], tiles='cartodbpositron', zoom_start=2)
 item = top_range[9]
 
@@ -120,6 +121,24 @@ for i in range(0,len(df)):
         color=color_producer(df.iloc[i][item])).add_to(m_3)
 
 print ('Price of: ', item)
+
+
+# Price of:  Toyota Corolla 1.6l 97kW Comfort (Or Equivalent New Car)
+m_4= folium.Map(location=[df.lat.mean(),df.lon.mean()], tiles='cartodbpositron', zoom_start=2)
+item = 'Toyota Corolla 1.6l 97kW Comfort (Or Equivalent New Car)'
+
+# Add a bubble map to the base map
+for i in range(0,len(df)):
+    Circle(
+        location=[df.iloc[i]['lat'], df.iloc[i]['lon']],
+        radius=1000,
+        color=color_producer(df.iloc[i][item])).add_to(m_4)
+
+print ('Price of: ', item)
+
+
+
+
 
 
 
